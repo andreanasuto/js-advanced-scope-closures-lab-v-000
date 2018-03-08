ffa8b6f53cd2fe36f080565dc469af7e440d2f09
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-08 19:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function produceDrivingRange(range) {
   function ciccio(start, end) {
     let distance = Math.abs(start.substring(0, 2) - end.substring(0, 2))
     if (distance > range) {
+      return `${distance - range} blocks out of range`
+    } else {
       return `within range by ${distance - range}`
     }
   }
