70decd343b3aae11888f6980a619684b5fe67d09
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-08 19:31:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function produceDrivingRange(range) {
+  return function ciccio(start, end) {
+    let distance = Math.abs(start.substring(0, 2) - end.substring(0, 2))
+    if (distance > range) {
+      return `${distance - range} blocks out of range`
+    } else {
+      return `within range by ${range - distance}`
+    }
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function tip(ridePrice) {
+    return ridePrice * percentage
+  }
+}
+
+
+function createDriver() {
+  let idDriver = 0
+
+  return class Driver {
+    constructor(name) {
+      this.name = name
+      this.id = idDriver++
+    }
+  }
+}
