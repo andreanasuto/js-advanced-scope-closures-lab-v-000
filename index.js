75e2558f5d9a97fc61d03ae00155c0db8ae18ca0
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-08 19:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function produceDrivingRange(range) {
   function ciccio(start, end) {
-    Math.abs(start.substring(0, 2) - end.substring(0, 2))
+    return Math.abs(start.substring(0, 2) - end.substring(0, 2))
   }
 }
